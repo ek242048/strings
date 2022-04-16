85ba3f4c03cc54ad5cbd9b89ea422edb93362c80
Kate <ek242048@yandex.ru>
2022-04-16 18:28:47 +0500

    первый коммит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,23 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' '+ firstName + ' ' + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+    // Задание 2
+    String fullNameForReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameForReport);
+
+    //Задание 3
+        String fullName3 = "Иванов Семён Семёнович";
+        fullName3 = fullName3.replace("ё","е" );
+        System.out.println("Данные ФИО сотрудника — " + fullName3);
+
+
     }
+
 }
